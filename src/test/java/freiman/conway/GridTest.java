23e324e7c0f2e7c2c84df419601f744ff93d4dc9
Anna Freiman <annafrei924@gmail.com>
2024-09-10 12:34:18 -0400

    Fix checkstyles

--- src/test/java/freiman/conway/GridTest.java
+++ src/test/java/freiman/conway/GridTest.java
@@ -1,11 +1,8 @@
 package freiman.conway;
+
 import org.junit.jupiter.api.Test;
-
-import java.util.Random;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+
 
 public class GridTest {
 
@@ -24,11 +21,11 @@ public class GridTest {
     @Test
     public void nextGen() {
         //given
-        Grid grid = new Grid(3,3);
-        grid.put(0,0);
-        grid.put(1,0);
-        grid.put(2,1);
-        grid.put(2,2);
+        Grid grid = new Grid(3, 3);
+        grid.put(0, 0);
+        grid.put(1, 0);
+        grid.put(2, 1);
+        grid.put(2, 2);
 
         //when
         grid.nextGen();
