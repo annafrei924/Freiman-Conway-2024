61731dc5f544e3ade991133589f797da83b573e8
Anna Freiman <annafrei924@gmail.com>
2024-09-10 12:54:03 -0400

    Fix checkstyles

--- src/main/java/freiman/conway/Grid.java
+++ src/main/java/freiman/conway/Grid.java
@@ -26,7 +26,7 @@ public class Grid {
                 int neighbors = 0;
                 for (int i = -1; i < 2; i++) {
                     for (int j = -1; j < 2; j++) {
-                        if (i == 0 && j == 0){
+                        if (i == 0 && j == 0) {
                             continue;
                         }
                         if((y + i >= 0 && y + i < getHeight()) && (x + j >= 0 && x + j < getWidth())) {
@@ -38,9 +38,9 @@ public class Grid {
                 }
 
                 //cell dies from loneliness or overpopulation
-                if (field[y][x] == 1 && (neighbors < 2 || neighbors >= 4)){
+                if (field[y][x] == 1 && (neighbors < 2 || neighbors >= 4)) {
                     next[y][x] = 0;
-                } else if (field[y][x] == 0 && neighbors == 3){
+                } else if (field[y][x] == 0 && neighbors == 3) {
                     next[y][x] = 1;
                 } else {
                     next[y][x] = field[y][x];
