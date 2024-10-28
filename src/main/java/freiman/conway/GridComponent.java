3d6ab3622533e7081100922dbdc911f5a4164c5b
Anna Freiman <126410515+annafrei924@users.noreply.github.com>
2024-10-28 13:54:49 +0200

    Update GridComponent.java

--- src/main/java/freiman/conway/GridComponent.java
+++ src/main/java/freiman/conway/GridComponent.java
@@ -7,62 +7,31 @@ import java.awt.event.MouseListener;
 
 public class GridComponent extends JComponent {
     private final Grid grid;
-    private final int gridSpacing = 15;
+    private final int cellSize;
 
-
-    public GridComponent(Grid grid) {
+    public GridComponent(Grid grid, int cellSize) {
         this.grid = grid;
-        addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                int posX = e.getX() / gridSpacing;
-                int posY = e.getY() / gridSpacing;
-                if (!grid.isAlive(posX, posY)) {
-                    grid.put(posX, posY);
-                } else {
-                    grid.remove(posX, posY);
-                }
+        this.cellSize = cellSize;
 
-                repaint();
-
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
-        });
+        grid.setStart();
     }
 
-    @Override
+@Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.LIGHT_GRAY);
-        g.fillRect(0, 0, grid.getWidth() * gridSpacing, grid.getHeight() * gridSpacing);
-        //draw the boxes
+        
         for (int y = 0; y < grid.getHeight(); y++) {
             for (int x = 0; x < grid.getWidth(); x++) {
                 if (grid.isAlive(x, y)) {
                     g.setColor(Color.GREEN);
-                    g.fillRect(x * gridSpacing, y * gridSpacing, gridSpacing, gridSpacing);
+                    g.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
                 }
             }
         }
+    }
 
+    public int getCellSize()
+    {
+        return cellSize;
     }
 }
